2439bc4ff977f817246a8fd98224b896f55306e8
Olecsandr <olecsandri07@gmail.com>
2025-08-05 01:55:13 +0300

    dfas

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+    form: document.querySelector(".form"),
+    input: document.querySelector('input[name="search-text"]'),
+    gallery: document.querySelector(".gallery"),
+    loader: document.querySelector(".loader-box"),
+    loadBtn: document.querySelector(".load-more-btn"),
+}
